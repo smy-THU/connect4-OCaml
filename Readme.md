39a2674c0a710ed2d68459f2713b6f2927a76ac6
Shunchi Zhang <shunchizhang.cs@gmail.com>
2024-12-06 22:25:20 -0500

    add test coverage

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Web Connect4 Game in OCaml
+
+
--- src/run.sh
+++ src/run.sh
@@ -1,2 +1,11 @@
+echo "Clean and build the project ..."
 dune clean && dune build
+
+
+echo "Test codes and coverage ..."
+cd ..
+dune test && bisect-ppx-report html && bisect-ppx-report summary
+cd -
+
+echo "Launch live demo ..."
 dune exec ./bin/connect4.exe
